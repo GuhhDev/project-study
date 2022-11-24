cd48f83c142872bf335932bb0641bc025e0a645d
Gustavo Henrique Batista <89535864+GuhhDev@users.noreply.github.com>
2022-11-24 20:05:14 -0300

    Added project via upload

---
+++ project/src/test/java/com/gustavo/project/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
